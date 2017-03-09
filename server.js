45c08ad0b64b6473a6d8ac760abb1da7f3a317bb
Rodrigo Ali <rodrigojali@gmail.com>
2017-03-09 20:40:52 +0000

    updates made in server.js, first try

--- server.js
+++ server.js
@@ -1,46 +1,25 @@
-'use strict';
-
-var path = require('path');
-var moment = require('moment');
 var express = require('express');
+var useragent = require('useragent');
 
 var app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  var file = path.join(__dirname, 'index.html');
-  res.sendFile(file, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent: ', file);
-    }
+  var userAgent = useragent.parse(req.headers['user-agent']);
+  var userIp = req.headers["x-forwarded-for"];
+
+  if (userIp){
+    var list = userIp.split(",");
+    userIp = list[list.length-1];
+  } else {
+    userIp = req.connection.remoteAddress;
+  }
+
+  res.json({
+    ip: userIp,
+    lang: req.headers['accept-language'].split(',')[0],
+    os: userAgent.os.family
   });
-});
-
-app.get('/:datestring', function(req,res) {
-  var myDate;
-  //we ask for the parameter's nature: if it is an unix number or date string
-  if(/^\d{8,}$/.test(req.params.datestring)) {
-    myDate = moment(req.params.datestring, "X"); //parse to UNIX
-  } else {
-    myDate = moment(req.params.datestring, "MMMM D, YYYY"); //parse to Natural 
-  }
-
-  // we set the response json to the formated timestamp or null if it's not a valid moment date
-  if(myDate.isValid()) {
-    res.json({
-      unix: myDate.format("X"),
-      natural: myDate.format("MMMM D, YYYY")
-    });
-  } else {
-    res.json({
-      unix: null,
-      natural: null
-    });
-  }
 });
 
 app.listen(port, function(){
